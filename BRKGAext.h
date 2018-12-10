196fd7d99d4500725f8fc910daa3f012aa9ed5f7
Rafael Louback Ferraz <ferrazrafael@yahoo.com.br>
2018-12-10 18:03:07 -0200

    minor fixes

--- BRKGAext.h
+++ BRKGAext.h
@@ -174,8 +174,9 @@ template< class Decoder, class RNG >
 void BRKGAext<Decoder, RNG>::diversifyElite() {
 	// for each population check if elite is to homogeneous
 	for(unsigned i = 0; i < K; ++i) {
-		// auxiliary 'pe' that uses adaptive pe if adaptiveParameters are on or default pe otherwise
 		unsigned _pe = adaptiveParameters ? adpPe[i] : pe;
+		// auxiliary 'pe' that uses adaptive pe if adaptiveParameters are on or default pe otherwise
+
 		double avg = eliteAverage(*current[i], _pe);
 		double percent = (eliteStandardDeviation(*current[i], avg, _pe) * 100.0) / current[i]->getBestFitness() / 100.0;
 
