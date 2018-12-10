fcdd3d1546633d895a7be8deb8f8490b6ee618ff
Rafael Louback Ferraz <ferrazrafael@yahoo.com.br>
2018-12-10 16:55:16 -0200

    Minor changes related to adaptive parameters

--- BRKGAext.h
+++ BRKGAext.h
@@ -27,14 +27,15 @@ public:
 	// Elite diversification
 	void useEliteDiversification(bool on, double diversityRate = 0.05);
 
-	// Initialize population based on initial solution
-	void initializeNonRandom(std::function<std::vector<unsigned> (unsigned)> initFunc);
-
 	// Apply Heuristic implemented by user
 	template <typename ItemType>
 	void applyHeuristic(std::function< std::vector<ItemType> (const std::vector<ItemType>&) > heuristic);
+	
+	// Initialize population based on initial solution
+	void initializeNonRandom(std::function< std::vector<unsigned> (unsigned) > initFunc);
 
 protected:
+	// Just to shorten base class variable use
 	using BRKGA<Decoder, RNG>::n;
 	using BRKGA<Decoder, RNG>::p;
 	using BRKGA<Decoder, RNG>::pe;
@@ -58,10 +59,10 @@ protected:
 	std::vector<double> adpRhoe;
 
 	void diversifyElite();
+	double average(const Population& population, unsigned auxPe);
+	double standardDeviation(const Population& population, double avg, unsigned auxPe);
 	void updateAdaptiveParameters();
 	void adaptiveEvolution(Population& curr, Population& next, const unsigned k);
-	double average(const Population& population);
-	double standardDeviation(const Population& population, double avg);
 	std::vector<double> generateChromosome(const std::vector<unsigned>& permutation);
 };
 
@@ -101,8 +102,169 @@ void BRKGAext< Decoder, RNG >::evolve(unsigned generations) {
 	}
 }
 
+template<class Decoder, class RNG>
+void BRKGAext<Decoder, RNG>::useAdaptiveParameters(bool on) {
+	adaptiveParameters = on;
+}
+
+template<class Decoder, class RNG>
+void BRKGAext<Decoder, RNG>::useEliteDiversification(bool on, double _diversityRate) {
+	eliteDiversification = on;
+	diversityRate = _diversityRate;
+}
+
+template<class Decoder, class RNG>
+template<typename ItemType>
+void BRKGAext<Decoder, RNG>::applyHeuristic(std::function< std::vector<ItemType> (const std::vector<ItemType>&) > heuristic) {
+	for(int i = 0; i < int(K); ++i) {
+		for(int j = 0; j < int(p); ++j) {
+			std::vector<ItemType> solution = refDecoder.decodeSolution((*current[i])(j)) ;
+			solution = heuristic(solution);
+			refDecoder.correctChromosome((*current[i])(j), solution);
+			current[i]->setFitness(j, refDecoder.decode((*current[i])(j)));
+		}
+	}
+}
+
 template< class Decoder, class RNG >
-inline void BRKGAext< Decoder, RNG >::adaptiveEvolution(Population& curr, Population& next, const unsigned k) {
+void BRKGAext< Decoder, RNG >::initializeNonRandom(std::function<std::vector<unsigned> (unsigned)> initFunc) {
+	using std::range_error;
+
+	for(unsigned i = 0; i < K; ++i) {
+		for(unsigned j = 0; j < p; ++j) {
+			std::vector<unsigned> permutation = initFunc(n);
+			if(permutation.size() != n) {
+				throw range_error("Permutation size differs from chromosome size.");
+			}
+
+			(*current[i])(j) = generateChromosome(permutation);
+		}
+
+		// Decode:
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(MAX_THREADS)
+#endif
+		for(int j = 0; j < int(p); ++j) {
+			current[i]->setFitness(j, refDecoder.decode((*current[i])(j)) );
+		}
+
+		// Sort:
+		current[i]->sortFitness();
+	}
+}
+
+template< class Decoder, class RNG >
+std::vector<double> BRKGAext< Decoder, RNG >::generateChromosome(const std::vector<unsigned>& permutation) {
+	std::vector<double> chromosome(n);
+	std::vector<double> keys(n);
+
+	for(unsigned i = 0; i < n; ++i) {
+		keys[i] = refRNG.rand();
+	}
+	std::sort(keys.begin(), keys.end());
+
+	for(unsigned i = 0; i < n; ++i) {
+		chromosome[i] = keys[permutation[i]];
+	}
+
+	return chromosome;
+}
+
+template< class Decoder, class RNG >
+void BRKGAext<Decoder, RNG>::diversifyElite() {
+	// for each population check if elite is to homogeneous
+	for(unsigned i = 0; i < K; ++i) {
+		// auxiliary 'pe' that uses adaptive pe if adaptiveParameters are on or default pe otherwise
+		unsigned auxPe = adaptiveParameters ? adpPe[i] : pe;
+		double avg = average(*current[i], auxPe);
+		double percent = (standardDeviation(*current[i], avg, auxPe) * 100.0) / current[i]->getBestFitness() / 100.0;
+
+		if(percent < diversityRate) {
+			// replace elite chromosomes that are below diversity rate with mutants
+			for(unsigned j = 1; j < auxPe; ++j) {
+				double percentualDeviation = fabs( (current[i]->getFitness(j) - avg) / avg );
+
+				if(percentualDeviation < diversityRate) {
+					// generating mutant chromosome
+					for(unsigned k = 0; k < n; ++k) { (*current[i])(j, k) = refRNG.rand(); }
+				}
+			}
+			// Time to compute fitness, in parallel:
+#ifdef _OPENMP
+#pragma omp parallel for num_threads(MAX_THREADS)
+#endif
+			for(int j = 1; j < int(auxPe); ++j) {
+				current[i]->setFitness( j, refDecoder.decode(current[i]->population[i]) );
+			}
+
+			// Now we must sort 'current' by fitness, since things might have changed:
+			current[i]->sortFitness();
+		}
+	}
+}
+
+template< class Decoder, class RNG >
+double BRKGAext<Decoder, RNG>::average(const Population& population, unsigned auxPe) {
+	double avg = 0.0;
+	for(unsigned i = 0; i < auxPe; ++i) {
+		avg += population.getFitness(i);
+	}
+	avg /= double(auxPe); // divide sum of all fitness by p
+	return avg;
+}
+
+template< class Decoder, class RNG >
+double BRKGAext<Decoder, RNG>::standardDeviation(const Population& population, double avg, unsigned auxPe) {
+	double sum = 0.0;
+	for(unsigned i = 0; i < auxPe; ++i) {
+		sum += population.getFitness(i) - avg;
+	}
+	sum /= double(auxPe); // divide sum of all fitness by p
+
+	return sqrt(sum);
+}
+
+template< class Decoder, class RNG >
+void BRKGAext<Decoder, RNG>::updateAdaptiveParameters() {
+	// update parameters
+	for(unsigned i = 0; i < K; ++i) {
+		double avgFitness = 0.0;
+		for(unsigned j = 0; j < p; ++j) {
+			avgFitness += current[i]->getFitness(j);
+		}
+		avgFitness /= double(p); // divide sum of all fitness by p
+
+		double bestFitness = current[i]->getBestFitness();
+
+		// generating adaptive pm's
+		double k = (pm / double(p)) / 2;
+		double avg = 0.0;
+		for(unsigned j = 0; j < p; ++j) {
+			avg += fabs(k * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness)) ;
+		}
+		avg /= double(p); // divide sum of all Pm's by p
+		adpPm[i] = unsigned(p * avg);
+
+		// generating adaptive pe's
+		k = (pe / double(p)) / 2;
+		avg = 0.0;
+		for(unsigned j = 0; j < p; ++j) {
+			avg += fabs( k * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness) );
+		}
+		avg /= double(p); // divide sum of all Pm's by p
+		adpPe[i] = unsigned(p * avg);
+
+		// generating adaptive rhoe's
+		avg = 0.0;
+		for(unsigned j = 0; j < p; ++j) {
+			avg += 0.5 + fabs( 0.25 * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness) );
+		}
+		adpRhoe[i] = avg / double(p); // divide sum of all Rhoe's by p
+	}
+}
+
+template< class Decoder, class RNG >
+void BRKGAext< Decoder, RNG >::adaptiveEvolution(Population& curr, Population& next, const unsigned k) {
 	// We now will set every chromosome of 'current', iterating with 'i':
 	unsigned i = 0;	// Iterate chromosome by chromosome
 	unsigned j = 0;	// Iterate allele by allele
@@ -150,169 +312,6 @@ inline void BRKGAext< Decoder, RNG >::adaptiveEvolution(Population& curr, Popula
 
 	// Now we must sort 'current' by fitness, since things might have changed:
 	next.sortFitness();
-}
-
-template<class Decoder, class RNG>
-void BRKGAext<Decoder, RNG>::useAdaptiveParameters(bool on) {
-	adaptiveParameters = on;
-}
-
-template<class Decoder, class RNG>
-void BRKGAext<Decoder, RNG>::useEliteDiversification(bool on, double _diversityRate) {
-	eliteDiversification = on;
-	diversityRate = _diversityRate;
-}
-
-template< class Decoder, class RNG >
-double BRKGAext<Decoder, RNG>::average(const Population& population) {
-	double avg = 0.0;
-	for(unsigned i = 0; i < pe; ++i) {
-		avg += population.getFitness(i);
-	}
-	avg /= double(pe); // divide sum of all fitness by p
-	return avg;
-}
-
-template< class Decoder, class RNG >
-double BRKGAext<Decoder, RNG>::standardDeviation(const Population& population, double avg) {
-	double sum = 0.0;
-	for(unsigned i = 0; i < pe; ++i) {
-		sum += population.getFitness(i) - avg;
-	}
-	sum /= double(pe); // divide sum of all fitness by p
-
-	return sqrt(sum);
-}
-
-template< class Decoder, class RNG >
-void BRKGAext<Decoder, RNG>::diversifyElite() {
-	// for each population check if elite is to homogeneous
-	for(unsigned i = 0; i < K; ++i) {
-		double avg = average(*current[i]);
-		double percent = (standardDeviation(*current[i], avg) * 100.0) / current[i]->getBestFitness() / 100.0;
-
-		if(percent < diversityRate) {
-			// replace elite chromosomes that are below diversity rate with mutants
-			for(unsigned j = 1; j < pe; ++j) {
-				double percentualDeviation = fabs( (current[i]->getFitness(j) - avg) / avg );
-
-				if(percentualDeviation < diversityRate) {
-					// generating mutant chromosome
-					for(unsigned k = 0; k < n; ++k) { (*current[i])(j, k) = refRNG.rand(); }
-				}
-			}
-			// Time to compute fitness, in parallel:
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(MAX_THREADS)
-#endif
-			for(int j = 1; j < int(pe); ++j) {
-				current[i]->setFitness( j, refDecoder.decode(current[i]->population[i]) );
-			}
-
-			// Now we must sort 'current' by fitness, since things might have changed:
-			current[i]->sortFitness();
-		}
-	}
-}
-
-template< class Decoder, class RNG >
-void BRKGAext<Decoder, RNG>::updateAdaptiveParameters() {
-	// update parameters
-	for(unsigned i = 0; i < K; ++i) {
-		double avgFitness = 0.0;
-		for(unsigned j = 0; j < p; ++j) {
-			avgFitness += current[i]->getFitness(j);
-		}
-		avgFitness /= double(p); // divide sum of all fitness by p
-
-		double bestFitness = current[i]->getBestFitness();
-
-		// generating adaptive pm's
-		double k = (pm / double(p)) / 2;
-		double avg = 0.0;
-		for(unsigned j = 0; j < p; ++j) {
-			avg += fabs(k * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness)) ;
-		}
-		avg /= double(p); // divide sum of all Pm's by p
-		adpPm[i] = unsigned(p * avg);
-
-		// generating adaptive pe's
-		k = (pe / double(p)) / 2;
-		avg = 0.0;
-		for(unsigned j = 0; j < p; ++j) {
-			avg += fabs( k * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness) );
-		}
-		avg /= double(p); // divide sum of all Pm's by p
-		adpPe[i] = unsigned(p * avg);
-
-		// generating adaptive rhoe's
-		avg = 0.0;
-		for(unsigned j = 0; j < p; ++j) {
-			avg += 0.5 + fabs( 0.25 * (current[i]->getFitness(j) - bestFitness) / (avgFitness - bestFitness) );
-		}
-		adpRhoe[i] = avg / double(p); // divide sum of all Rhoe's by p
-
-		/*std::cout << "Pm: " << adpPm[i] << "  ";
-		std::cout << "Pe: " << adpPe[i] << "  ";
-		std::cout << "Rhoe: " << adpRhoe[i] << std::endl;*/
-	}
-}
-
-template<class Decoder, class RNG>
-template<typename ItemType>
-void BRKGAext<Decoder, RNG>::applyHeuristic(std::function< std::vector<ItemType> (const std::vector<ItemType>&) > heuristic) {
-	for(int i = 0; i < int(K); ++i) {
-		for(int j = 0; j < int(p); ++j) {
-			std::vector<ItemType> solution = refDecoder.decodeSolution((*current[i])(j)) ;
-			solution = heuristic(solution);
-			refDecoder.correctChromosome((*current[i])(j), solution);
-			current[i]->setFitness(j, refDecoder.decode((*current[i])(j)));
-		}
-	}
-}
-
-template< class Decoder, class RNG >
-std::vector<double> BRKGAext< Decoder, RNG >::generateChromosome(const std::vector<unsigned>& permutation) {
-	std::vector<double> chromosome(n);
-	std::vector<double> keys(n);
-
-	for(unsigned i = 0; i < n; ++i) {
-		keys[i] = refRNG.rand();
-	}
-	std::sort(keys.begin(), keys.end());
-
-	for(unsigned i = 0; i < n; ++i) {
-		chromosome[i] = keys[permutation[i]];
-	}
-
-	return chromosome;
-}
-
-template< class Decoder, class RNG >
-void BRKGAext< Decoder, RNG >::initializeNonRandom(std::function<std::vector<unsigned> (unsigned)> initFunc) {
-	using std::range_error;
-
-	for(unsigned i = 0; i < K; ++i) {
-		for(unsigned j = 0; j < p; ++j) {
-			std::vector<unsigned> permutation = initFunc(n);
-			if(permutation.size() != n) {
-				throw range_error("Permutation size differs from chromosome size.");
-			}
-
-			(*current[i])(j) = generateChromosome(permutation);
-		}
-
-		// Decode:
-#ifdef _OPENMP
-#pragma omp parallel for num_threads(MAX_THREADS)
-#endif
-		for(int j = 0; j < int(p); ++j) {
-			current[i]->setFitness(j, refDecoder.decode((*current[i])(j)) );
-		}
-
-		// Sort:
-		current[i]->sortFitness();
-	}
 }
 
 #endif /* BRKGAEXT_H_ */
